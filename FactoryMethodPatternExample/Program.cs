65749e5c0444ec7c3c5ed5fde4d13a77697af1ec
Ayan Sinha <ayansinha333@gmail.com>
2026-06-21 20:36:14 +0530

    Week 1: Singleton and Factory Method patterns

---
+++ FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,55 @@
+﻿abstract class Document
+{
+    public abstract void Open();
+}
+
+class WordDocument : Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening Word Document");
+    }
+}
+
+class PdfDocument : Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening PDF Document");
+    }
+}
+
+abstract class DocumentFactory
+{
+    public abstract Document CreateDocument();
+}
+
+class WordDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DocumentFactory factory1 = new WordDocumentFactory();
+        Document doc1 = factory1.CreateDocument();
+        doc1.Open();
+
+        DocumentFactory factory2 = new PdfDocumentFactory();
+        Document doc2 = factory2.CreateDocument();
+        doc2.Open();
+    }
+}
