33c3b7aed44596b3eba5d2f1b194c629b9ac8a2e
Ayan Sinha <ayansinha333@gmail.com>
2026-07-05 14:18:49 +0530

    Completed Module 4 - Moq Hands-on

---
+++ Module-4-NUnit-Moq/CustomerCommLib/CustomerCommLib.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using CustomerCommLib;
+
+namespace CustomerCommLib.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestSendMail()
+        {
+            var mockMailSender = new Mock<IMailSender>();
+
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                          .Returns(true);
+
+            CustomerComm customer = new CustomerComm(mockMailSender.Object);
+
+            bool result = customer.SendMailToCustomer("test@gmail.com"); ;
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ Module-4-NUnit-Moq/CustomerCommLib/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer(string email)
+        {
+            return _mailSender.SendMail(email, "Welcome Customer");
+        }
+    }
+}
