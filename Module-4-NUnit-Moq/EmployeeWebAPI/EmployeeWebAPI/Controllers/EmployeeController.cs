485bb99c0095d92937495d711596d4e76322d487
Ayan Sinha <ayansinha333@gmail.com>
2026-07-05 16:49:28 +0530

    Completed Module 6 - Employee Web API

---
+++ Module-4-NUnit-Moq/EmployeeWebAPI/EmployeeWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Employee List");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Employee Id : {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Employee Added");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"Employee {id} Updated");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Employee {id} Deleted");
+        }
+    }
+}
