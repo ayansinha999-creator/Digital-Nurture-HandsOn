afdfb671b5ff850da380b28dd30e585eafce85d2
Ayan Sinha <ayansinha333@gmail.com>
2026-07-05 16:28:43 +0530

    Completed Module 5 - First ASP.NET Core Web API

---
+++ Module-4-NUnit-Moq/FirstWebAPI/FirstWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to First Web API");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Employee Added");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Employee Updated");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Employee Deleted");
+        }
+    }
+}
