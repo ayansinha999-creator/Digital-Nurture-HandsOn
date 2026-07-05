759ed754a3fc674fa8736e9d30a36f18b4c72415
Ayan Sinha <ayansinha333@gmail.com>
2026-07-05 13:12:05 +0530

    Completed Module 4 - NUnit Unit Testing

---
+++ Module-4-NUnit-Moq/NUnitHandsOn/NUnitHandsOn/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace NUnitHandsOn
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
